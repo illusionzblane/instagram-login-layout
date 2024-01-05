75c340607db7c6b3c5528a863af3ec6413b79406
Brian Paragas <brianparagas49@gmail.com>
2024-01-04 17:13:38 -0800

    adding new files and folders

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div>
-          Test
+          Instagram
         </div>
       </main>
     </>
